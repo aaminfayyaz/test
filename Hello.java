c84baf794577fd834d062eadbaaa11130f03c79d
Fayyaz Mohammad <15111771+aaminfayyaz@users.noreply.github.com>
2019-12-11 19:16:07 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
 	public static void main(String[] args){
 	System.out.println("Welcome to Hello World! ");
-	for (int i=1	;i<=5;i++){
+	for (int i=1	;i<=10;i++){
 		System.out.println("Iterating : " +i);
 	}
 	}
