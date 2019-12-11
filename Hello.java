7f2cafe1d03ea2430164ce473091539a0cfcfdc1
Fayyaz Mohammad <15111771+aaminfayyaz@users.noreply.github.com>
2019-12-11 19:12:20 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+	public static void main(String[] args){
+	System.out.println("Welcome to Hello World! ");
+	for (int i=1	;i<=5;i++){
+		System.out.println("Iterating : " +i);
+	}
+	}
+}
+	
